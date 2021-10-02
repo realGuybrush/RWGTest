9601f1fa7b0da1d7290757571c0ac5817ee0a88c
realGuybrush <TripleBaka@yandex.ru>
2021-10-02 21:11:51 +0300

    Added two more types of enemies.

--- Test/Assets/Scripts/Gameplay/ShipControllers/CustomControllers/EnemyShipController.cs
+++ Test/Assets/Scripts/Gameplay/ShipControllers/CustomControllers/EnemyShipController.cs
@@ -10,11 +10,26 @@ public class EnemyShipController : ShipController
     [SerializeField]
     private Vector2 _fireDelay;
 
+    [SerializeField]
+    private float maxLatSpd;
+
+    private int spdRotCoeff = 0;
+
     private bool _fire = true;
     
     protected override void ProcessHandling(MovementSystem movementSystem)
     {
+        movementSystem.LateralMovement(Time.deltaTime * LatSpdCoeff());
         movementSystem.LongitudinalMovement(Time.deltaTime);
+    }
+
+    private float LatSpdCoeff()
+    {
+        float spdCoeff = maxLatSpd * Mathf.Sin(spdRotCoeff * Mathf.Deg2Rad);
+        spdRotCoeff+=5;
+        if (spdRotCoeff > 360)
+            spdRotCoeff = 0;
+        return spdCoeff;
     }
 
     protected override void ProcessFire(WeaponSystem fireSystem)
